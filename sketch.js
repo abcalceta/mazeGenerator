e56bb6012dd2f835669cbe8f2e5a8f55089924fd
frances calceta <frances.calceta@gmail.com>
2017-06-04 21:32:01 +0800

    made it nice on mobile

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 // sketch.js
 var canvas;
+var count;
 function setup(){
 	canvas = createCanvas(windowWidth,windowHeight);
 	canvas.position(0,0);
 	canvas.style('z-index',-1);
+	background(240,240,240);
+	count = 0;
 }
 
 function windowResized(){
@@ -13,6 +16,11 @@ function windowResized(){
 function draw(){
 	frameRate(10);
 	background(240,240,240,10);
-	fill(random(200,250),220,220,100); noStroke();
-	ellipse(random(1,windowWidth),random(1,windowHeight),300,300);
+	if (count%5 == 0){
+		var dark = random(150,250)
+		fill(random(200,250),dark,dark,80); noStroke();
+		var ellipseSize = random(200,350)
+		ellipse(random(1,windowWidth),random(1,windowHeight),ellipseSize,ellipseSize);
+	}
+	count+=1;
 }
